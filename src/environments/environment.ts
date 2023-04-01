7ba89d07052de600571dee4354c400fccaa9f391
Anil Yurtsever <admin>
2023-04-01 20:54:24 +0200

    feature
    env variable set

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  service_url: "http://37.59.230.225:8001"
 };
 
 /*
