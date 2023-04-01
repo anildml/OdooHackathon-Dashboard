faf13ba04aeb5e3a19c4bafed27c18cf2ce80671
Anil Yurtsever <admin>
2023-04-01 12:09:56 +0200

    feature
    component and page templates impl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { QuestionComponent } from './components/question/question.component';
+import { RoomComponent } from './components/room/room.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { QuestionsPageComponent } from './pages/questions-page/questions-page.component';
+import { RoomPageComponent } from './pages/room-page/room-page.component';
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {UserAuthHeaderInterceptor} from "./configs/auth-config/user-auth-header.interceptor";
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    QuestionComponent,
+    RoomComponent,
+    LoginPageComponent,
+    QuestionsPageComponent,
+    RoomPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: UserAuthHeaderInterceptor,
+      multi: true,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
