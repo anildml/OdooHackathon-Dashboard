190ffb2f69da057b964792eace0d4a45854c01ac
Anil Yurtsever <admin>
2023-04-01 13:40:44 +0200

    feature
    routes defined

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginPageComponent} from "./pages/login-page/login-page.component";
+import {DashboardPageComponent} from "./pages/dashboard-page/dashboard-page.component";
+import {RoomPageComponent} from "./pages/room-page/room-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '**', component: DashboardPageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'dashboard', component: DashboardPageComponent },
+  { path: 'room', component: RoomPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+
+}
