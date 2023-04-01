f6250e93cbb5cef4e8e0cd74f4ca1a7489f7f4c4
Anil Yurtsever <admin>
2023-04-01 16:21:15 +0200

    fix
    imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,14 @@ import { QuestionComponent } from './components/question/question.component';
 import { RoomComponent } from './components/room/room.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RoomPageComponent } from './pages/room-page/room-page.component';
-import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {UserAuthHeaderInterceptor} from "./configs/auth-config/user-auth-header.interceptor";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatCardModule} from "@angular/material/card";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -26,7 +30,12 @@ import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.co
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [
     {
