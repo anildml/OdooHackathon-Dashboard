6e9cc2cdb9bb6855a01a683b74bf5bbada627c46
pierrelouishance <pierrelouishance@gmail.com>
2023-04-02 11:30:06 +0200

    feature
    login page implementations

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output, EventEmitter,Input} from '@angular/core';
+import { UserService } from 'src/app/services/user-service/user.service';
+import { FormGroup, FormControl } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  
+  constructor(
+    private userService: UserService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
+  }
+  check_if_false=false;
+  async sendConnectionInfo(username: string,password : string) {
+    // Do something with the content here
+    console.log(username);
+    console.log(password);
+  
+    await this.userService.login(username, password);
+    let check_if_log = this.userService.isLoggedIn();
+
+    if (check_if_log){
+    this.router.navigate(['dashboard']);
+    }else{this.check_if_false=true;}
   }
 
 }
