419213aec17652a68630af42444e04a735b50dda
Anil Yurtsever <admin>
2023-04-02 11:17:58 +0200

    feature
    profile images randomized

--- src/app/components/room/room.component.ts
+++ src/app/components/room/room.component.ts
@@ -13,6 +13,14 @@ export class RoomComponent implements OnInit {
   questionsGrouped: any = {};
   questionCount = 1;
 
+  profileImages: string[] = [
+    "https://xsgames.co/randomusers/assets/avatars/male/46.jpg",
+    "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50",
+    "https://i.pinimg.com/originals/ae/ec/c2/aeecc22a67dac7987a80ac0724658493.jpg",
+    "https://preview.redd.it/profile-picture-of-the-random-old-guy-who-likes-your-photo-v0-5560va6tsg191.jpg?auto=webp&s=e9d18181cd0aebb733360843c9251c9853a132a8",
+    "https://blog.texasbar.com/files/2014/05/NickLaurent1.gif"
+  ];
+
   constructor(
     private roomService: RoomService,
     private planService: PlanService
@@ -50,6 +58,7 @@ export class RoomComponent implements OnInit {
 
   public getReplyList(): any[] {
     if (this.questionsGrouped[this.questionCount] != undefined) {
+      console.log(this.questionsGrouped[this.questionCount][0].replies);
       return this.questionsGrouped[this.questionCount][0].replies;
     }
     return [];
